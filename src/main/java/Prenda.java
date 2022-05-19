125d656a6defdcac5ab41d835cf9e2fc4baa852d
DaianaKaneshiro <daiana.kane@gmail.com>
2022-05-19 20:48:30 -0300

    agregados

--- src/main/java/Prenda.java
+++ src/main/java/Prenda.java
@@ -1,19 +1,32 @@
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+
 public class Prenda {
   TipoDePrenda tipo;
   MaterialPrenda material;
   Color colorPrincipal;
   Color colorSecundario;
+  Trama trama;
+
   public Prenda(TipoDePrenda tipo, MaterialPrenda material,Color colorPrincipal,Color colorSecundario){
-        if(tipo == null || material == null ||colorPrincipal== null ){
+     /*   if(tipo == null || material == null ||colorPrincipal== null ){
             throw new ExcepcionPrendaInvalida("Faltan datos requeridos");
-        }
+        }*/
     this.tipo=tipo;
     this.material=material;
     this.colorPrincipal=colorPrincipal;
-    this.colorSecundario=colorSecuandario;
+    this.colorSecundario=colorSecundario;
     }
 }
-enum Categoria {
+//quito las validaciones de la prenda xq las voy a validar en el build (borrador) de la iteracion 2
+class Categoria {
+  TipoCategoria tipoCategoria;
+  public Categoria (TipoCategoria tipoCategoria) {
+    this.tipoCategoria = tipoCategoria;
+  }
+}
+enum TipoCategoria {
   PARTE_SUPERIOR,
   CALZADO,
   PARTE_INFERIOR,
@@ -41,6 +54,44 @@ class TipoDePrenda {
     return this.categoria;
    }
 }
+//-------------------------------- ITERACION 2-----------------
+//Como usuarie de QuéMePongo, quiero especificar qué trama tiene la tela de una prenda (lisa, rayada, con lunares, a cuadros o un estampado).
+enum Trama{
+  LISA, RAYADA, LUNARES, CUADROS, ESTAMPADO,
+}
 
+class Borrador {
+  TipoDePrenda tipo;
+  MaterialPrenda material;
+  Color colorPrincipal;
+  Color colorSecundario;
+  Trama trama= Trama.LISA; //quiero poder no indicar ninguna trama para una tela, y que por defecto ésta sea lisa.
 
+  //de esta manera creo la prenda en pasos
+  public void especificarTipo(TipoDePrenda tipo){
+    requireNonNull(tipo);
+    this.tipo=tipo;
+  }
+  public void especificarMaterial(MaterialPrenda material){
+    requireNonNull(material);
+    this.material=material;
+  }
+  public void especificarColorPrincipal(Color colorPrincipal){
+    requireNonNull(colorPrincipal);
+    this.colorPrincipal=colorPrincipal;
+  }
+  public void especificarColorSecundario(Color colorSecundario){
+    requireNonNull(colorSecundario);
+    this.colorSecundario=colorSecundario;
+  }
+  public void especificarTrama(Trama trama){
+    this.trama=trama;
+  }
+  public Prenda crearPrenda() {
+    return new Prenda(tipo, material, colorPrincipal, colorSecundario,trama);
+  }
+  //requireNonNull para el fail fast
 
+}
+--------------------------------------------------------------------------------------------------
+Iteracion 4
